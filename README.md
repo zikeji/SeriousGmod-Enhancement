d192e64a9d7c3aad85d7a9426003dabc6d9f23c2
Caleb Blankemeyer <caleb@zynados.com>
2015-12-30 06:18:59 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# SeriousGmod-Enhancement
-Browser extension to add various enhancements to the SeriousGmod community forums.
+SeriousGmod Enhancement
+=============
+
+This repo contains the current source to the SeriousGmod Enhancement Extension built on top of the [Kango Extensions framework](http://kangoextensions.com/).
+
+Features
+-----
+0. Redirect HTTP to HTTPS
+0. Display the current Steam username of a user in relevant areas.
+
+Planned
+-----
+0. Menu to toggle settings
+0. Multiple display formats for displaying Steam user info
+
+Compiling
+-----
+To compile this yourself, please download the [Kango Extensions framework](http://kangoextensions.com/) and setup your environment. Also ensure you have git installed and properly configured.
+
+1. Open up your terminal (or git bash on Windows) and navigate to the ```kango-framework-latest``` folder (you downloaded this).
+2. ```python kango.py create "../SeriousGmodEnhancement" "SeriousGmod Enhancement"```
+3. ```cd ../SeriousGmodEnhancement/src/common```
+4. ```rm -rf *``` (clean the source dir so we can clone the project)
+4. ```git clone https://github.com/zikeji/SeriousGmod-Enhancement.git```
+5. Run the build.cmd or build.sh which can be found in the SeriousGmodEnhancement main directory you created in step 2.
---
+++ main.js
@@ -0,0 +1,37 @@
+﻿// ==UserScript==
+// @name            SeriousGmod Enhancement
+// @author          Zikeji
+// @match           https://www.seriousgmod.com/*
+// @match           http://www.seriousgmod.com/*
+// @require         jquery-2.1.4.min.js
+// ==/UserScript==
+'use strict';
+
+var info = kango.getExtensionInfo();
+
+if (window.location.protocol != "https:")
+    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+
+var steamIDs = [];
+$('a[href*="http://steamcommunity.com/profiles/"] img[src="styles/default/steam/steam_16.png"]').parent().each(function(i, ele) {
+    t_arr = ele.href.split('/');
+    sID = t_arr[t_arr.length - 1];
+    if ($.inArray(sID, steamIDs) === -1) steamIDs.push(sID);
+});
+
+kango.xhr.send({
+    method: 'GET',
+    url: 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + info.SteamAPIKey + '&steamids=' + steamIDs.join(','),
+    contentType: 'json'
+}, function(data) {
+    if (data.status == 200 && data.response != null) {
+        if (typeof data.response.response.players != "undefined") {
+            players = data.response.response.players
+            for (var i in players) {
+                player = players[i];
+                $('a[href="http://steamcommunity.com/profiles/' + player.steamid + '"] img[src="styles/default/steam/steam_16.png"]').css('vertical-align', '-3px');
+                $('a[href="http://steamcommunity.com/profiles/' + player.steamid + '"] img[src="styles/default/steam/steam_16.png"]').parent().append("&nbsp;(" + player.personaname + ")");
+            }
+        }
+    }
+});
